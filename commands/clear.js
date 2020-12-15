6b4101e73a8219aacc6483d816055a9839fde165
김용주 <yongjoo0924@gmail.com>
2020-12-15 16:30:46 +0900

    fin

--- commands/clear.js
+++ commands/clear.js
@@ -1,16 +1,16 @@
 module.exports = {
     name: "지우기",
-    description: "Clears messages",
+    description: "청소기능",
 
     async run (client, message, args) {
 
         const amount = args.join(" ");
 
-        if(!amount) return message.reply('please provide an amount of messages for me to delete')
+        if(!amount) return message.reply('삭제할 메세지 양을 입력해주세요')
 
-        if(amount > 100) return message.reply(`you cannot clear more than 100 messages at once`)
+        if(amount > 100) return message.reply(`100자 이상의 글자를 삭제할 수 없습니다`)
 
-        if(amount < 1) return message.reply(`you need to delete at least one message`)
+        if(amount < 1) return message.reply(`최소 1개의 메세지를 삭제해야합니다`)
 
         await message.channel.messages.fetch({limit: amount}).then(messages => {
             message.channel.bulkDelete(messages
